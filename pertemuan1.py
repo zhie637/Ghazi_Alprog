703712b4b826dce7b81bd7167722da872d47dc3e
zhie637 <Ghazirayyanalghifari03@gmail.com>
2026-04-08 09:53:25 +0800

    Add files via upload

---
+++ pertemuan1.py
@@ -0,0 +1,14 @@
+nama = input("masukkan nama anda:")
+if nama=="ghazi":
+   print("selamat datang:")
+umur = int(input("masukkan umur anda:"))
+
+if umur == 19:
+   print("anda cukup umur")
+elif umur == 0:
+   print("anda belum lahir")   
+elif umur == 15:
+   print("anda belum cukup umur")
+elif umur == 80:
+   print("anda sudah bau tanah,perbanyak amal ibadah")
+   print("program selesai")
